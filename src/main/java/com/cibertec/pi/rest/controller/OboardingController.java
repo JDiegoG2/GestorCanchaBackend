a1d1664393b9b66e605e6ab6a9d5c9dd4dbed4f3
Roy <royjaimes411@gmail.com>
2024-07-30 22:41:04 -0500

    fix: add cors

--- src/main/java/com/cibertec/pi/rest/controller/OboardingController.java
+++ src/main/java/com/cibertec/pi/rest/controller/OboardingController.java
@@ -12,11 +12,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class OboardingController {
 
     @Autowired
